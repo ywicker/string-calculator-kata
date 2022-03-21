6a1106fcf17dec2a288ef96c24083c3b0ba4ee73
ywick <yoann.wicker@lacombedulionvert.fr>
2022-03-21 19:35:30 +0100

    init

---
+++ src/test/java/lacombe/kata/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package lacombe.kata;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+    @Test
+    void projectInit () {
+        Assertions.assertTrue(true);
+    }
+}
