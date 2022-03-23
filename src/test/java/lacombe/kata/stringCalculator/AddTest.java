e25520755ad649c0c98fe9862652a257196d5627
ywick <yoann.wicker@lacombedulionvert.fr>
2022-03-23 17:14:02 +0100

    step 2 - refactoring : adding setup method to instantiate stringCalculator

--- src/test/java/lacombe/kata/stringCalculator/AddTest.java
+++ src/test/java/lacombe/kata/stringCalculator/AddTest.java
@@ -2,17 +2,23 @@ package lacombe.kata.stringCalculator;
 
 import lacombe.kata.errors.IsOtherThanANumberException;
 import lacombe.kata.StringCalculator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class AddTest {
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setup() {
+        this.stringCalculator = new StringCalculator();
+    }
+
     @Test
     @DisplayName("An empty string must return 0")
     void anEmptyStringMustReturn0 () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("");
 
         assertThat(value).isEqualTo(0);
@@ -20,8 +26,6 @@ public class AddTest {
     @Test
     @DisplayName("A string with zero must return 0")
     void aStringWithZeroMustReturn0 () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("0");
 
         assertThat(value).isEqualTo(0);
@@ -29,8 +33,6 @@ public class AddTest {
     @Test
     @DisplayName("A string with a single positive number must return that number")
     void aStringWithASinglePositiveNumberMustReturnThatNumber () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("1");
 
         assertThat(value).isEqualTo(1);
@@ -38,8 +40,6 @@ public class AddTest {
     @Test
     @DisplayName("A string with a positive number and zero must return the positive number")
     void aStringWithAPositiveNumberAndZeroMustReturnThePositiveNumber () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("1,0");
 
         assertThat(value).isEqualTo(1);
@@ -47,8 +47,6 @@ public class AddTest {
     @Test
     @DisplayName("A string with zero and a positive number must return the positive number")
     void aStringWithZeroAndAPositiveNumberMustReturnThePositiveNumber () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("0,1");
 
         assertThat(value).isEqualTo(1);
@@ -56,8 +54,6 @@ public class AddTest {
     @Test
     @DisplayName("A string with two positive numbers must return the sum of those")
     void aStringWithTwoPositiveNumbersMustReturnTheSumOfThose () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("1,1");
 
         assertThat(value).isEqualTo(2);
@@ -65,8 +61,6 @@ public class AddTest {
     @Test
     @DisplayName("A string with two negative numbers must return the sum of those")
     void aStringWithTwoNegativeNumbersMustReturnTheSumOfThose () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("-1,-1");
 
         assertThat(value).isEqualTo(-2);
@@ -74,16 +68,12 @@ public class AddTest {
     @Test
     @DisplayName("A string other than a number raises an exception")
     void aStringOtherThanANumberRaisesAnException (){
-        var stringCalculator = new StringCalculator();
-
         assertThatThrownBy(() -> stringCalculator.add("0,a"))
                 .isInstanceOf(IsOtherThanANumberException.class);
     }
     @Test
     @DisplayName("A string with multiple positive numbers must return the sum of those")
     void aStringWithMultiplePositiveNumbersMustReturnTheSumOfThose () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("1,2,3,4,5");
 
         assertThat(value).isEqualTo(15);
@@ -91,8 +81,6 @@ public class AddTest {
     @Test
     @DisplayName("A string with multiple positive and negative numbers must return the sum of those")
     void aStringWithMultiplePositiveAndNegativeNumbersMustReturnTheSumOfThose () throws IsOtherThanANumberException {
-        var stringCalculator = new StringCalculator();
-
         int value = stringCalculator.add("-1,2,-3,4,-5");
 
         assertThat(value).isEqualTo(-3);
